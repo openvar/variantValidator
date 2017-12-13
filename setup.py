2451a8aa539d7703e2aca29642f933a033f6ae5e
Peter Causey-Freeman <pjf9@le.ac.uk>
2017-12-13 09:33:54 +0000

    final restructuring for pip install version

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     url='',
     author='Peter J. Causey-Freeman',
     author_email='pjf9@leicester.ac.uk',
-    packages=['variantValidator',],
-    license='University of Leicester VariantValidator License',
+    packages=find_packages(),
+    license='University of Leicester VariantValidator License (LICENSE.txt)',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
@@ -30,7 +30,16 @@ setup(
     ],
  
     # What does your project relate to?
-    keywords='sample setuptools development',
+      keywords=[
+          "bioinformatics",
+          "computational biology",
+          "genome variants",
+          "genome variation",
+          "genomic variants",
+          "genomic variation",
+          "genomics",
+          "hgvs",
+      ],
 
 	# List run-time dependencies here.  These will be installed by pip when the project is installed.
     install_requires=[
@@ -40,6 +49,6 @@ setup(
 		"configparser >= 3.5.0",
 		"pyliftover >= 0.3",
 		"biotools >= 0.3.0",
-		"mysql_connector >=	2.1.4",  
+		# "mysql_connector >=	2.1.4",  Required but is OS specific. https://dev.mysql.com/downloads/connector/python/
     ],
 )
