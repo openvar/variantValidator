d045964cee61e056cfb02dbeefed40f1e79cd6c1
Peter Causey-Freeman <pjf9@le.ac.uk>
2018-02-22 14:40:07 +0000

    Ensure the transcript variants have reference bases using normalization prior to multi genomic variant myvm t_to_g mapping

--- variantValidator/variantValidator.py
+++ variantValidator/variantValidator.py
@@ -5368,13 +5368,6 @@ def validator(batch_variant, selected_assembly, select_transcripts):
 								ori = va_func.tx_exons(tx_ac=hgvs_coding.ac, alt_ac=alt_chr, alt_aln_method=alt_aln_method, hdp=hdp)
 								orientation = int(ori[0]['alt_strand'])								
 								hgvs_alt_genomic = va_func.myvm_t_to_g(hgvs_coding, alt_chr, vm, hn, hdp, primary_assembly)
-					
-					
-								print 'YIPPERS!'
-								print hgvs_coding
-								print hgvs_alt_genomic								
-								
-								
 								# genomic_possibilities
 								# 1. take the simple 3 pr normalized hgvs_genomic
 								# 2. Lock in hgvs_genomic at its most 5 prime position wrt genome
