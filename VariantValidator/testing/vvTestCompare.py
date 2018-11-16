f422e508e6c90b12fdfcf423273ca3791bc62ef0
pjdp2 <pjdp2@le.ac.uk>
2018-11-16 12:30:04 +0000

    Bore the gree

--- VariantValidator/testing/vvTestCompare.py
+++ VariantValidator/testing/vvTestCompare.py
@@ -5,12 +5,12 @@ import vvTestFunctions as fn
 masterDirectory="outputs101"
 testDirectory1="outputs246"
 testDirectory2="outputsITS"
-testDirectory2="outputs249"
+testDirectory3="outputs249"
 
 print("Comparing "+masterDirectory+" and "+testDirectory1)
 fn.compareBatches(masterDirectory,testDirectory1)
 print("Comparing "+masterDirectory+" and "+testDirectory2)
 fn.compareBatches(masterDirectory,testDirectory2)
 print("Comparing "+masterDirectory+" and "+testDirectory3)
-fn.compareBatches(masterDirectory,testDirectory2)
+fn.compareBatches(masterDirectory,testDirectory3)
 
