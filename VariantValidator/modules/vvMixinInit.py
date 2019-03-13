1aec8bf0beb7bcccd568950c2b9407ffc5b47b87
TeriForey <teri.forey@gmail.com>
2019-03-13 09:11:27 +0000

    Tidyied up the configuration reading in the MixinInit

--- VariantValidator/modules/vvMixinInit.py
+++ VariantValidator/modules/vvMixinInit.py
@@ -1,5 +1,5 @@
 import os
-from configparser import ConfigParser,RawConfigParser
+from configparser import ConfigParser
 import hgvs
 import hgvs.parser
 import hgvs.dataproviders.uta
@@ -17,16 +17,14 @@ from Bio.Seq import Seq
 
 import re
 import copy
-#import io
 from .vvDatabase import vvDatabase
 from .vvLogging import logger
 from . import vvFunctions as fn
 from VariantValidator.configure import CONFIG_DIR
 
 
-
-class Mixin():
-    '''
+class Mixin:
+    """
     # This object contains configuration options for the validator, but it inherits the mixin
     # class in vvCore that contains the enormous validator function.
 
@@ -44,7 +42,7 @@ class Mixin():
     val=Validator()
     val.validate("some kind of gene situation","The genome version","the transcripts to use")
 
-    '''
+    """
     def __init__(self):
         '''
         Renaming of variables :
@@ -66,14 +64,14 @@ class Mixin():
         # The custom vvLogging module will set itself up using the VALDIATOR_DEBUG environment variable.
         levelString = config["logging"]['level']
         consoleString = config["logging"]['console']
-        if consoleString.lower()=="true":
-            consoleString="console"
+        if consoleString.lower() == "true":
+            consoleString = "console"
         fileString = config["logging"]['file']
-        if fileString.lower()=="true":
-            fileString="file"
+        if fileString.lower() == "true":
+            fileString = "file"
         traceString = config["logging"]['trace']
-        if traceString.lower()=="true":
-            traceString="trace"
+        if traceString.lower() == "true":
+            traceString = "trace"
         logString = levelString+" "+consoleString+" "+fileString+" "+traceString
         os.environ["VALIDATOR_DEBUG"] = logString
 
@@ -83,7 +81,7 @@ class Mixin():
         self.seqrepoPath = os.path.join(config["seqrepo"]["location"], self.seqrepoVersion)
         os.environ['HGVS_SEQREPO_DIR'] = self.seqrepoPath
 
-        os.environ['UTA_DB_URL']= "postgresql://%s:%s@%s/%s/%s" % (
+        os.environ['UTA_DB_URL'] = "postgresql://%s:%s@%s/%s/%s" % (
             config["postgres"]["user"],
             config["postgres"]["password"],
             config['postgres']['host'],
@@ -93,92 +91,97 @@ class Mixin():
         self.utaPath = os.environ.get('UTA_DB_URL')
 
         self.dbConfig = {
-            'user':    config["mysql"]["user"],
-            'password':config["mysql"]["password"],
-            'host':    config["mysql"]["host"],
-            'database':config["mysql"]["database"],
+            'user':     config["mysql"]["user"],
+            'password': config["mysql"]["password"],
+            'host':     config["mysql"]["host"],
+            'database': config["mysql"]["database"],
     	    'raise_on_warnings': True
         }
         #Create database access objects
-        self.db=vvDatabase(self,self.dbConfig)
+        self.db = vvDatabase(self, self.dbConfig)
         # Set up versions
         __version__ = config["variantValidator"]['version']
-        self.version=__version__
-        if re.match('^\d+\.\d+\.\d+$', __version__) is not None:
-            self.releasedVersion=True
+        self.version = __version__
+        if re.match(r'^\d+\.\d+\.\d+$', __version__) is not None:
+            self.releasedVersion = True
             _is_released_version = True
         else:
-            self.releasedVersion=False
-        self.hgvsVersion=hgvs.__version__
+            self.releasedVersion = False
+        self.hgvsVersion = hgvs.__version__
 
         # Set up other configuration variables
-        self.liftoverPath=config["liftover"]["location"]
+        self.liftoverPath = config["liftover"]["location"]
         if not self.liftoverPath == 'PATH/TO/LIFTOVER':
-            os.environ['PYLIFTOVER_DIR']=self.liftoverPath
-        self.entrezID=config["EntrezID"]['entrezid']
+            os.environ['PYLIFTOVER_DIR'] = self.liftoverPath
+        self.entrezID = config["EntrezID"]['entrezid']
 
         # Set up HGVS
         # Configure hgvs package global settings
         hgvs.global_config.uta.pool_max = 25
         hgvs.global_config.formatting.max_ref_length = 1000000
+
         # Create HGVS objects
         self.hdp = hgvs.dataproviders.uta.connect(pooling=True)
-        self.hp = hgvs.parser.Parser() #Parser
-        self.vr = hgvs.validator.Validator(self.hdp) # Validator
-        self.vm = hgvs.variantmapper.VariantMapper(self.hdp) # Variant mapper
+        self.hp = hgvs.parser.Parser()  # Parser
+        self.vr = hgvs.validator.Validator(self.hdp)  # Validator
+        self.vm = hgvs.variantmapper.VariantMapper(self.hdp)  # Variant mapper
+
         # Create a lose vm instance
         self.lose_vm = hgvs.variantmapper.VariantMapper(self.hdp,
-                                                   replace_reference=True,
-                                                   prevalidation_level=None
-                                                   )
-        self.nr_vm = hgvs.variantmapper.VariantMapper(self.hdp, replace_reference=False) #No reverse variant mapper
-        self.sf = hgvs.dataproviders.seqfetcher.SeqFetcher() # Seqfetcher
+                                                        replace_reference=True,
+                                                        prevalidation_level=None
+                                                        )
+
+        self.nr_vm = hgvs.variantmapper.VariantMapper(self.hdp, replace_reference=False)  # No reverse variant mapper
+        self.sf = hgvs.dataproviders.seqfetcher.SeqFetcher()  # Seqfetcher
+
         # Set standard genome builds
         self.genome_builds = ['GRCh37', 'hg19', 'GRCh38']
         self.utaSchema = str(self.hdp.data_version())
 
         # Create normalizer
         self.reverse_hn = hgvs.normalizer.Normalizer(self.hdp,
-                                                cross_boundaries=False,
-                                                shuffle_direction=5,
-                                                alt_aln_method='splign'
-                                                )
+                                                     cross_boundaries=False,
+                                                     shuffle_direction=5,
+                                                     alt_aln_method='splign'
+                                                     )
 
         # Create normalizer
         self.merge_normalizer = hgvs.normalizer.Normalizer(self.hdp,
-                                                      cross_boundaries=False,
-                                                      shuffle_direction=hgvs.global_config.normalizer.shuffle_direction,
-                                                      alt_aln_method='splign',
-                                                      validate=False
-                                                      )
+                                                           cross_boundaries=False,
+                                                           shuffle_direction=hgvs.global_config.normalizer.shuffle_direction,
+                                                           alt_aln_method='splign',
+                                                           validate=False
+                                                           )
         self.reverse_merge_normalizer = hgvs.normalizer.Normalizer(self.hdp,
-                                                              cross_boundaries=False,
-                                                              shuffle_direction=hgvs.global_config.normalizer.shuffle_direction,
-                                                              alt_aln_method='splign',
-                                                              validate=False
-                                                              )
+                                                                   cross_boundaries=False,
+                                                                   shuffle_direction=hgvs.global_config.normalizer.shuffle_direction,
+                                                                   alt_aln_method='splign',
+                                                                   validate=False
+                                                                   )
         #create no_norm_evm
         self.no_norm_evm_38 = hgvs.assemblymapper.AssemblyMapper(self.hdp,
-                                                            assembly_name='GRCh38',
-                                                            alt_aln_method='splign',
-                                                            normalize=False,
-                                                            replace_reference=True
-                                                            )
+                                                                 assembly_name='GRCh38',
+                                                                 alt_aln_method='splign',
+                                                                 normalize=False,
+                                                                 replace_reference=True
+                                                                 )
 
         self.no_norm_evm_37 = hgvs.assemblymapper.AssemblyMapper(self.hdp,
-                                                            assembly_name='GRCh37',
-                                                            alt_aln_method='splign',
-                                                            normalize=False,
-                                                            replace_reference=True
-                                                            )
+                                                                 assembly_name='GRCh37',
+                                                                 alt_aln_method='splign',
+                                                                 normalize=False,
+                                                                 replace_reference=True
+                                                                 )
 
     def __del__(self):
         del self.db
+
     def myConfig(self):
-        '''
-        #Returns configuration:
-        #version, hgvs version, uta schema, seqrepo db.
-        '''
+        """
+        Returns configuration:
+        version, hgvs version, uta schema, seqrepo db.
+        """
         return {
             'variantvalidator_version': self.version,
             'variantvalidator_hgvs_version': self.hgvsVersion,
@@ -214,6 +217,7 @@ class Mixin():
             var_p.ac = 'Non-coding transcript'
             var_p.posedit = ''
             return var_p
+
     def myc_to_p(self,hgvs_transcript, evm, re_to_p):
         # Create dictionary to store the information
         hgvs_transcript_to_hgvs_protein = {'error': '', 'hgvs_protein': '', 'ref_residues': ''}
