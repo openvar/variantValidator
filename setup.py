08e35cc49af383c82bc9cb496c8d7507e6d363e0
TeriForey <teri.forey@gmail.com>
2019-03-20 19:01:25 +0000

    Updated version to 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VariantValidator',
-    version='0.2.2',
+    version='0.2.3',
     description='API for accurate, mapping and formatting of sequence variants using HGVS nomenclature',
     long_description=open('README.md').read(),
     url='',
