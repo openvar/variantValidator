857fc63c103ebb9ec33bfc2e21f69724430dbb03
TeriForey <teri.forey@gmail.com>
2019-07-15 15:26:46 +0100

    Tidying up validator.py

--- VariantValidator/__init__.py
+++ VariantValidator/__init__.py
@@ -2,6 +2,6 @@ from . import configure
 from . import logger
 from .version import __version__
 
-from .variantValidator import Validator
+from .validator import Validator
 
 __all__ = ["Validator"]
--- VariantValidator/validator.py
+++ VariantValidator/validator.py
@@ -1,19 +1,8 @@
 from .modules import vvMixinCore as vvMixinCore
 
-class Validation():
-    '''
-    #Validation objects contain a number of variant interpretations
-    '''
-    pass
-
-class ValOutput():
-    '''
-    #This object contains a single possible interpretation of a variant
-    '''
-    pass
 
 class Validator(vvMixinCore.Mixin):
-    '''
+    """
     #Mixins are used to split this very large, complex object over multiple files.
     #There is a logical chain to it, though:
     # vvMixinInit
@@ -23,7 +12,7 @@ class Validator(vvMixinCore.Mixin):
     # vvMixinCore
     #     v
     # Validator    <- this object.
-    '''
+    """
     pass
 
 
