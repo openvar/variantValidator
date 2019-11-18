6e84aaa3b74e2e518c8d3029f71186fb2e7489f4
Peter Causey-Freeman <pjf9@leicester.ac.uk>
2019-11-18 13:08:07 +0000

    Add __future__ imports to version because Travis complained

--- VariantValidator/version.py
+++ VariantValidator/version.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 import pkg_resources
 import re
 import warnings
@@ -11,3 +12,20 @@ try:
 except pkg_resources.DistributionNotFound as e:
     warnings.warn("can't get __version__ because %s package isn't installed" % __package__, Warning)
     __version__ = None
+
+# <LICENSE>
+# Copyright (C) 2019 VariantValidator Contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# </LICENSE>
