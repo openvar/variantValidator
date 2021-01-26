75af78520d2d35d2a0d65eeaca08f2191eda0314
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2021-01-26 15:44:08 +0000

    added tests which report failure if the output is not what is expected

--- VariantValidator/modules/exon_numbering_tests.py
+++ VariantValidator/modules/exon_numbering_tests.py
@@ -8,22 +8,35 @@ This code runs tests on exon_numbering.py to check the outputs are as expected
 """
 from exon_numbering import finds_exon_number, request_sequence
 
-#Testing
 #define some variants to test with 
-test_variant_2 = "NM_007294.3:c.1067A>G"
-test_variant_1  = 'NM_000088.3:c.642+1GG>G'
-test_variant_3 = "NM_000088.3:c.642+1G>A"
-test_variant_4 = "NM_000094.3:c.6751-3_6751-2del"
-test_variant_5 = "NM_007294.3:c.5426-2del"
-test_variant_6 = "NM_000088.3:c.589G>T" #this is an exon boundary and maps to 715
-test_variant_7 = "NM_000088.3:c.642del" #this should map to 768
-test_variant_8 = "NM_000094.3:c.6751-3_6753del" #starts in intron, ends in exon
+test_variant_1 = "NM_007294.3:c.1067A>G"
+test_variant_2 = "NM_000088.3:c.642+1G>A"
+test_variant_3 = "NM_000094.3:c.6751-3_6751-2del"
+test_variant_4 = "NM_000088.3:c.589G>T" #this is an exon boundary and maps to 715
+test_variant_5 = "NM_000088.3:c.642del" #this should map to 768
+test_variant_6 = "NM_000094.3:c.6751-3_6753del" #starts in intron, ends in exon
 
-#test for our variant
-#finds_exon_number(test_variant_2)
-#print(finds_exon_number(test_variant_1))
-#print(finds_exon_number(test_variant_3))
-#print(finds_exon_number(test_variant_4))
-#print(finds_exon_number(test_variant_5))
-#print(finds_exon_number(test_variant_5))
-print(finds_exon_number(test_variant_8))
+#run the tests, reporting failure if the output is not what is expected
+test_1 = finds_exon_number(test_variant_1)
+if test_1['NC_000017.10']['start_exon'] != '14' and test_1['NG_005905.2']['end_exon'] != '10':
+    print("Failed: test variant 1")
+
+test_2 = finds_exon_number(test_variant_2)
+if test_2['NC_000017.10']['start_exon'] != '44i' and test_2['NG_007400.1']['end_exon'] != '8i':
+    print("Failed: test variant 2")
+
+test_3 = finds_exon_number(test_variant_3)
+if test_3['NC_000003.11']['start_exon'] != '32i' and test_3['NG_007065.1']['end_exon'] != '85i':
+    print("Failed: test variant 3")
+
+test_4 = finds_exon_number(test_variant_4)
+if test_4['NC_000017.10']['start_exon'] != '44' and test_4['NG_007400.1']['end_exon'] != '8':
+    print("Failed: test variant 4")
+
+test_5 = finds_exon_number(test_variant_5)
+if test_5['NC_000017.10']['start_exon'] != '44' and test_5['NG_007400.1']['end_exon'] != '8':
+    print("Failed: test variant 5")
+
+test_6 = finds_exon_number(test_variant_6)
+if test_6['NC_000003.11']['start_exon'] != '32i' and test_6['NC_000003.11']['end_exon'] != '33':
+    print("Failed: test variant 6")
