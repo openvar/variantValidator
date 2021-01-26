0e64d67dbd2453bf8b90821239ca84f9e5043d57
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2021-01-26 15:45:17 +0000

    deleted test variants, they are now stored in exon_numbering_tests.py

--- VariantValidator/modules/exon_numbering.py
+++ VariantValidator/modules/exon_numbering.py
@@ -128,28 +128,6 @@ def finds_exon_number(variant):
         exon_start_end_positions[transcript] = {"start_exon": start_exon, "end_exon": end_exon}
     return exon_start_end_positions
 
-#Testing
-#define some variants to test with 
-test_variant_2 = "NM_007294.3:c.1067A>G"
-test_variant_1  = 'NM_000088.3:c.642+1GG>G'
-test_variant_3 = "NM_000088.3:c.642+1G>A"
-test_variant_4 = "NM_000094.3:c.6751-3_6751-2del"
-test_variant_5 = "NM_007294.3:c.5426-2del"
-test_variant_6 = "NM_000088.3:c.589G>T" #this is an exon boundary and maps to 715
-test_variant_7 = "NM_000088.3:c.642del" #this should map to 768
-test_variant_8 = "NM_000094.3:c.6751-3_6753del" #starts in intron, ends in exon
-
-#test for our variant
-#print(finds_exon_number(test_variant_2))
-#print(finds_exon_number(test_variant_1))
-#print(finds_exon_number(test_variant_3))
-#print(finds_exon_number(test_variant_4))
-#print(finds_exon_number(test_variant_5))
-#print(finds_exon_number(test_variant_5))
-#print(finds_exon_number(test_variant_8))
-
-
-
 # <LICENSE>
 # Copyright (C) 2021 VariantValidator Contributors
 #
