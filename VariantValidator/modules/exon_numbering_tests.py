7cabdbcff3382e1ed86579e1fb863a461e583a11
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2021-01-27 15:00:53 +0000

    now uses unittest to automate testing

--- VariantValidator/modules/exon_numbering_tests.py
+++ VariantValidator/modules/exon_numbering_tests.py
@@ -6,7 +6,9 @@ Authors: Katie Williams (@kwi11iams) and Katherine Winfield (@kjwinfield)
 This code runs tests on exon_numbering.py to check the outputs are as expected
 
 """
+import unittest # This is the automatic test runner
 from exon_numbering import finds_exon_number, request_sequence, check_variant
+
 
 # Define some variants to test with
 test_variant_1 = "NM_007294.3:c.1067A>G"
@@ -16,35 +18,43 @@ test_variant_4 = "NM_000088.3:c.589G>T"  # An exon boundary, maps to 715
 test_variant_5 = "NM_000088.3:c.642del"  # This should map to 768
 test_variant_6 = "NM_000094.3:c.6751-3_6753del"  # Intron start, exon end
 
-def test_exon_numbering():
+
+class TestExonNumbering(unittest.TestCase):
     """
-    Function to automate running the tests, reporting failure if the output
-    is not what is expected
+    Class TextExonNumbering automates running the tests, and reports failure if
+    the output is not what is expected
     """
-    test_1 = finds_exon_number(test_variant_1)
-    assert (test_1['NC_000017.10']['start_exon'] == '14' and
-            test_1['NG_005905.2']['end_exon'] == '10'), "Failed test variant 1"
-    
-    test_2 = finds_exon_number(test_variant_2)
-    assert (test_2['NC_000017.10']['start_exon'] == '44i' and
-            test_2['NG_007400.1']['end_exon'] == '8i'), "Failed test variant 2"
+    def test_1(self):
+        test_1 = finds_exon_number(test_variant_1)
+        self.assertEqual(test_1['NC_000017.10']['start_exon'],
+                         '14', "Failed test variant 1")
 
-    test_3 = finds_exon_number(test_variant_3)
-    assert (test_3['NC_000003.11']['start_exon'] == '32i' and
-            test_3['NG_007065.1']['end_exon'] == '85i'), "Failed test variant 3"
+    def test_2(self):    
+        test_2 = finds_exon_number(test_variant_2)
+        self.assertEqual(test_2['NC_000017.10']['start_exon'],
+                         '44i', "Failed test variant 2")
 
-    test_4 = finds_exon_number(test_variant_4)
-    assert (test_4['NC_000017.10']['start_exon'] == '44' and
-            test_4['NG_007400.1']['end_exon'] == '8'), "Failed test variant 4"
+    def test_3(self):
+        test_3 = finds_exon_number(test_variant_3)
+        self.assertEqual(test_3['NC_000003.11']['start_exon'],
+                         '32i', "Failed test variant 3")
 
-    test_5 = finds_exon_number(test_variant_5)
-    assert (test_5['NC_000017.10']['start_exon'] == '44' and
-            test_5['NG_007400.1']['end_exon'] == '8'), "Failed test variant 5"
+    def test_4(self):
+        test_4 = finds_exon_number(test_variant_4)
+        self.assertEqual(test_4['NC_000017.10']['start_exon'],
+                         '44', "Failed test variant 4")
 
-    test_6 = finds_exon_number(test_variant_6)
-    assert (test_6['NC_000003.11']['start_exon'] == '32i' and
-            test_6['NC_000003.11']['end_exon'] == '33'), "Failed test variant 6"
+    def test_5(self):
+        test_5 = finds_exon_number(test_variant_5)
+        self.assertEqual(test_5['NC_000017.10']['start_exon'],
+                         '44', "Failed test variant 5")
+
+    def test_6(self):
+        test_6 = finds_exon_number(test_variant_6)
+        self.assertEqual(test_6['NC_000003.11']['start_exon'],
+                         '32i', "Failed test variant 6")
+
 
 if __name__ == "__main__":
-    test_exon_numbering()
+    unittest.main()
     print("Everything passed")
