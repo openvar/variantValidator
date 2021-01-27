8f4447277542ef850158c4a27b022aa071e6f1a4
kwi11iams <katie.williams-5@postgrad.manchester.ac.uk>
2021-01-27 11:39:47 +0000

    Added check using check_variant function I just created in exon_numbering.py

--- VariantValidator/modules/exon_numbering_tests.py
+++ VariantValidator/modules/exon_numbering_tests.py
@@ -6,17 +6,21 @@ Authors: Katie Williams (@kwi11iams) and Katherine Winfield (@kjwinfield)
 This code runs tests on exon_numbering.py to check the outputs are as expected
 
 """
-from exon_numbering import finds_exon_number, request_sequence
+from exon_numbering import finds_exon_number, request_sequence, check_variant
 
-#define some variants to test with 
+# Define some variants to test with
 test_variant_1 = "NM_007294.3:c.1067A>G"
 test_variant_2 = "NM_000088.3:c.642+1G>A"
 test_variant_3 = "NM_000094.3:c.6751-3_6751-2del"
-test_variant_4 = "NM_000088.3:c.589G>T" #this is an exon boundary and maps to 715
-test_variant_5 = "NM_000088.3:c.642del" #this should map to 768
-test_variant_6 = "NM_000094.3:c.6751-3_6753del" #starts in intron, ends in exon
+test_variant_4 = "NM_000088.3:c.589G>T"  # An exon boundary, maps to 715
+test_variant_5 = "NM_000088.3:c.642del"  # This should map to 768
+test_variant_6 = "NM_000094.3:c.6751-3_6753del"  # Starts in intron, ends in exon
 
-#run the tests, reporting failure if the output is not what is expected
+# Try check_variant function
+check_variant(test_variant_1, "GRCh38")   # This should work
+check_variant("NM_007294.3:c.1067T>G", "GRCh38")  # This shouldn't work 
+
+# Run the tests, reporting failure if the output is not what is expected
 test_1 = finds_exon_number(test_variant_1)
 if test_1['NC_000017.10']['start_exon'] != '14' and test_1['NG_005905.2']['end_exon'] != '10':
     print("Failed: Test variant 1, " + test_variant_1)
