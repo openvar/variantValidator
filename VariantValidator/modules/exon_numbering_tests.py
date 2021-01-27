ad491cb4d2cc8760f3570d6b36eb3cf34870c1f6
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2021-01-27 14:33:33 +0000

    testing now uses assert statements

--- VariantValidator/modules/exon_numbering_tests.py
+++ VariantValidator/modules/exon_numbering_tests.py
@@ -16,51 +16,35 @@ test_variant_4 = "NM_000088.3:c.589G>T"  # An exon boundary, maps to 715
 test_variant_5 = "NM_000088.3:c.642del"  # This should map to 768
 test_variant_6 = "NM_000094.3:c.6751-3_6753del"  # Intron start, exon end
 
-# Run the tests, reporting failure if the output is not what is expected
-test_1 = finds_exon_number(test_variant_1)
-if (test_1['NC_000017.10']['start_exon'] != '14' and
-        test_1['NG_005905.2']['end_exon'] != '10'):
-    print("Failed: Test variant 1, " + test_variant_1)
-else:
-    print("Passed: Test Variant 1, " + test_variant_1)
-    print("Output dictionary: ")
-    print(test_1)
+def test_exon_numbering():
+    """
+    Function to automate running the tests, reporting failure if the output
+    is not what is expected
+    """
+    test_1 = finds_exon_number(test_variant_1)
+    assert (test_1['NC_000017.10']['start_exon'] == '14' and
+            test_1['NG_005905.2']['end_exon'] == '10'), "Failed test variant 1"
+    
+    test_2 = finds_exon_number(test_variant_2)
+    assert (test_2['NC_000017.10']['start_exon'] == '44i' and
+            test_2['NG_007400.1']['end_exon'] == '8i'), "Failed test variant 2"
 
-test_2 = finds_exon_number(test_variant_2)
-if (test_2['NC_000017.10']['start_exon'] != '44i'
-        and test_2['NG_007400.1']['end_exon'] != '8i'):
-    print("Failed: test variant 2")
-else:
-    print("Passed: Test Variant 2, " + test_variant_2)
-    print("Output dictionary: ")
-    print(test_2)
+    test_3 = finds_exon_number(test_variant_3)
+    assert (test_3['NC_000003.11']['start_exon'] == '32i' and
+            test_3['NG_007065.1']['end_exon'] == '85i'), "Failed test variant 3"
 
-test_3 = finds_exon_number(test_variant_3)
-if (test_3['NC_000003.11']['start_exon'] != '32i'
-        and test_3['NG_007065.1']['end_exon'] != '85i'):
-    print("Failed: test variant 3")
-    print("Output dictionary: ")
-    print(test_3)
+    test_4 = finds_exon_number(test_variant_4)
+    assert (test_4['NC_000017.10']['start_exon'] == '44' and
+            test_4['NG_007400.1']['end_exon'] == '8'), "Failed test variant 4"
 
-test_4 = finds_exon_number(test_variant_4)
-if (test_4['NC_000017.10']['start_exon'] != '44' and
-        test_4['NG_007400.1']['end_exon'] != '8'):
-    print("Failed: test variant 4")
-    print("Output dictionary: ")
-    print(test_4)
+    test_5 = finds_exon_number(test_variant_5)
+    assert (test_5['NC_000017.10']['start_exon'] == '44' and
+            test_5['NG_007400.1']['end_exon'] == '8'), "Failed test variant 5"
 
-test_5 = finds_exon_number(test_variant_5)
-if (test_5['NC_000017.10']['start_exon'] != '44' and
-        test_5['NG_007400.1']['end_exon'] != '8'):
-    print("Failed: test variant 5")
-    print("Output dictionary: ")
-    print(test_5)
+    test_6 = finds_exon_number(test_variant_6)
+    assert (test_6['NC_000003.11']['start_exon'] == '32i' and
+            test_6['NC_000003.11']['end_exon'] == '33'), "Failed test variant 6"
 
-test_6 = finds_exon_number(test_variant_6)
-if (test_6['NC_000003.11']['start_exon'] != '32i' and
-        test_6['NC_000003.11']['end_exon'] != '33'):
-    print("Failed: test variant 6")
-else:
-    print("Passed: Test Variant 6, " + test_variant_6)
-    print("Output dictionary: ")
-    print(test_6)
+if __name__ == "__main__":
+    test_exon_numbering()
+    print("Everything passed")
