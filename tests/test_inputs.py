d22021dbef4b01ca67b5eed3271cb56eab0e826a
John-F-Wagstaff <jfw16@leicester.ac.uk>
2021-01-27 17:11:52 +0000

    Add extra GRCh38 patch mappings to test_variant66

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -3127,7 +3127,11 @@ class TestVariantsAuto(TestCase):
             'hgvs_genomic_description': 'NW_004504303.2:g.302883_302888del',
             'vcf': {'chr': 'HG1595_PATCH', 'pos': '302871', 'ref': 'CCCGCCA', 'alt': 'C'}}}, {'hg19': {
             'hgvs_genomic_description': 'NW_004504303.2:g.302883_302888del',
-            'vcf': {'chr': 'NW_004504303.2', 'pos': '302871', 'ref': 'CCCGCCA', 'alt': 'C'}}}])
+            'vcf': {'chr': 'NW_004504303.2', 'pos': '302871', 'ref': 'CCCGCCA', 'alt': 'C'}}}, {'grch38': {
+            'hgvs_genomic_description': 'NW_018654719.1:g.32644_32646=', 
+            'vcf': {'chr': 'HG2047_PATCH', 'pos': '32644', 'ref': 'CCC', 'alt': 'CCC'}}}, {'hg38': {
+            'hgvs_genomic_description': 'NW_018654719.1:g.32644_32646=', 
+            'vcf': {'chr': 'NW_018654719.1', 'pos': '32644', 'ref': 'CCC', 'alt': 'CCC'}}}])
         assert results['NM_032790.3:c.126_128=']['primary_assembly_loci']['hg19'] == {
             'hgvs_genomic_description': 'NC_000012.11:g.122064785_122064790del',
             'vcf': {'chr': 'chr12', 'pos': '122064773', 'ref': 'CCCGCCA', 'alt': 'C'}}
