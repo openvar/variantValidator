1ec5972cf6a00b5689c5e5a687d3cb46833a60ef
RSWilson1 <robert.s.wilson1996@gmail.com>
2022-02-16 10:36:53 +0000

    Added new tests

--- VariantValidator/modules/test_expanded_repeats.py
+++ VariantValidator/modules/test_expanded_repeats.py
@@ -94,6 +94,60 @@ class TestExpandedRepeats(unittest.TestCase):
         assert my_variant.after_the_bracket == ""
         # checks nothing is after the bracket
 
+    def test_basic_syntax_2(self):
+        """
+        Test for handling basic syntax of ENSG variant string.
+        """
+        variant_str = "ENSG00000198947.15:g.1ACT[10]"
+        my_variant = tandem_repeats.TandemRepeats.parse_repeat_variant(
+                                    variant_str, "GRCh37", "all")
+        my_variant.check_transcript_type()
+        my_variant.reformat_reference()
+        my_variant.check_genomic_or_coding()
+        formatted = my_variant.reformat()
+        assert formatted == "ENSG00000198947.15:g.1ACT[10]"
+        assert my_variant.variant_str == "ENSG00000198947.15:g.1ACT[10]"
+        assert my_variant.ref_type == "Ensembl"
+        # checks correct transcript type
+        assert my_variant.reference == "ENSG00000198947.15"
+        # checks correct ref name
+        assert my_variant.variant_position == "1"
+        # checks correct position
+        assert my_variant.repeat_sequence == "ACT"
+        # checks repeat seq
+        assert my_variant.copy_number == "10"
+        # checks number of repeats is str and correct
+        assert my_variant.after_the_bracket == ""
+        # checks nothing is after the bracket
+
+
+    def test_basic_syntax_3(self):
+        """
+        Test for handling basic syntax of ENSG variant string.
+        """
+        variant_str = "ENST00000357033.8:c.13AC[2]"
+        my_variant = tandem_repeats.TandemRepeats.parse_repeat_variant(
+                                    variant_str, "GRCh37", "all")
+        my_variant.check_transcript_type()
+        my_variant.reformat_reference()
+        my_variant.check_genomic_or_coding()
+        formatted = my_variant.reformat()
+        assert formatted == "ENST00000357033.8:c.13_14insACAC"
+        assert my_variant.variant_str == "ENST00000357033.8:c.13AC[2]"
+        assert my_variant.ref_type == "Ensembl"
+        # checks correct transcript type
+        assert my_variant.reference == "ENST00000357033.8"
+        # checks correct ref name
+        assert my_variant.variant_position == "13"
+        # checks correct position
+        assert my_variant.repeat_sequence == "AC"
+        # checks repeat seq
+        assert my_variant.copy_number == "2"
+        # checks number of repeats is str and correct
+        assert my_variant.after_the_bracket == ""
+        # checks nothing is after the bracket
+
+
     def test_transcript_versions(self):
         """
         Test for handing transcript versions.
