c768325b4f3fe76ef74a87d2201ea2be7c363ad8
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2024-05-01 11:57:52 +0100

    code to add in gene symbols if tx variant exists but gene symbol is missing)

--- VariantValidator/modules/vvMixinCore.py
+++ VariantValidator/modules/vvMixinCore.py
@@ -1315,6 +1315,12 @@ class Mixin(vvMixinConverters.Mixin):
                     predicted_protein_variant_dict["lrg_tlr"] = ''
                     predicted_protein_variant_dict["lrg_slr"] = ''
 
+
+                # Add missing gene info which should be there (May have come from uncertain positions for example)
+                if variant.hgvs_transcript_variant != '' and variant.gene_symbol == '':
+                    variant.gene_symbol = self.db.get_gene_symbol_from_transcript_id(
+                        variant.hgvs_transcript_variant.split(":")[0])
+
                 # Add stable gene_ids
                 stable_gene_ids = {}
                 if variant.gene_symbol != '':
