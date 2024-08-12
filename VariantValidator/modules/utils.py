4623e0553407b8c303800ee9edb7f392c5f24fa5
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2024-08-12 12:50:05 +0100

    bug in utils. Replace a lowercase

--- VariantValidator/modules/utils.py
+++ VariantValidator/modules/utils.py
@@ -568,7 +568,7 @@ def three_to_one(seq):
         'Ala': 'A', 'Cys': 'C', 'Asp': 'D', 'Glu': 'E',
         'Phe': 'F', 'Gly': 'G', 'His': 'H', 'Ile': 'I',
         'Lys': 'K', 'Leu': 'L', 'Met': 'M', 'Asn': 'N',
-        'Pro': 'P', 'Gln': 'Q', 'Arg': 'R', 'ser': 'S',
+        'Pro': 'P', 'Gln': 'Q', 'Arg': 'R', 'Ser': 'S',
         'Thr': 'T', 'Val': 'V', 'Trp': 'W', 'Tyr': 'Y',
         'Ter': '*'}
 
