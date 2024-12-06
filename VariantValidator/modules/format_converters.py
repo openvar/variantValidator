d79c2edead18af8a4ced3b0bc38a5dce0a41c9ce
John-F-Wagstaff <jfw16@leicester.ac.uk>
2024-12-06 19:41:31 +0000

    Move input formatting before object creation point
    Input formatting that depends on having a text quibble from user input
    needs to be completed before we parse into a hgvs object. Prepare for
    centralising input parsing by moving this kind of formatting before the
    intended hgvs string->hgvs object parsing point (at the end of the
    initial_format_conversions function).

--- VariantValidator/modules/format_converters.py
+++ VariantValidator/modules/format_converters.py
@@ -70,6 +70,10 @@ def initial_format_conversions(variant, validator, select_transcripts_dict_plus_
     if toskip:
         return True
 
+    toskip = use_checking.pre_parsing_global_common_mistakes(variant)
+    if toskip:
+        return True
+
     toskip = indel_catching(variant, validator)
     if toskip:
         return True
